9b1f2a25d0e1dc578e086b120b5db52b4c26879c
psrct <sirachet.c@gmail.com>
2025-10-25 03:52:07 +0700

    success ansible_final -> set_motd and showrun commands

--- ansible_final.py
+++ ansible_final.py
@@ -1,20 +1,33 @@
-import subprocess
+import subprocess, json
 
-STUDENT_ID = "66070191"
-
-def showrun():
-    command = ['ansible-playbook', '-i', 'hosts.yaml', '-e', f'STUDENT_ID={STUDENT_ID}', 'newplaybook.yaml']
+def run_ansible(command):
     try:
-        result = subprocess.run(command, capture_output=True, text=True, check=True)
+        result = subprocess.run(command, capture_output=True, text=True)
+
         if 'failed=0' in result.stdout:
             print("Ansible successfully.")
+            print(result.stdout)
             return "ok"
         else:
             print("Ansible failed.")
             print(result.stderr)
             return "Error: Ansible"
-            
     except subprocess.CalledProcessError as e:
         print("Error running ansible-playbook command.")
         print(e.stderr)
         return "Error: Ansible"
+    
+def showrun(ip_address, student_id):
+    filename = f"show_run_{student_id}_{ip_address}.txt"
+    command = ['ansible-playbook', '-i', 'hosts.yaml', '--limit', ip_address, '-e', f'STUDENT_ID={student_id}', '-e', f'output_filename={filename}', 'show_run_playbook.yaml']
+    return run_ansible(command)
+
+def set_motd(ip_address, motd_text, student_id):
+    motd_message = f"{motd_text}"
+    command = [
+        'ansible-playbook', '-i', 'hosts.yaml', '--limit', ip_address,
+        '-e', json.dumps({"STUDENT_ID": student_id, "motd_message": motd_message}),
+        'motd_playbook.yaml'
+    ]
+    result = run_ansible(command)
+    return "Ok: success" if result == "ok" else result
